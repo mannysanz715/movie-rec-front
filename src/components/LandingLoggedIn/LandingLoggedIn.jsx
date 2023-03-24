60a1a6d4404c6513d60cdf93572d10bce7c6775d
mannysanz715 <mannysanz715@gmail.com>
2023-03-24 12:44:08 -0700

    Add Logic to Drop Down menu for genres

--- src/components/LandingLoggedIn/LandingLoggedIn.jsx
+++ src/components/LandingLoggedIn/LandingLoggedIn.jsx
@@ -1,28 +1,57 @@
 import * as movieService  from '../../services/movieService'
 import MovingShowing from '../../components/MovieShowing/MovieShowing'
-import { useState, useEffect } from 'react'
+import { useState } from 'react'
+import Select from 'react-select'
+
+const options = [
+  {value: '27', label : "Horror"},
+{value: '28', label : "Action"},
+{value: '12', label : "Adventure"},
+{value: '16', label : "Annimation"},
+{value: '35', label : "Comedy"},    
+{value: '80', label : "Crime"},
+{value: '99', label : "Documentary"},
+{value: '18', label : "Drama"},
+{value: '10751', label : "Family"},
+{value: '14', label : "Fantasy"},
+{value: '36', label : "History"},
+{value: '10402', label : "Music"},
+{value: '9648', label : "Mystery"},
+{value: '10749', label : "Romance"},
+{value: '878', label : "Sci Fi"},
+{value: '10770', label : "TV Movie"},
+{value: '53', label : "Thriller"},
+{value: '10752', label : "War"},
+{value: '37', label : "Western"},
+]
 
 function LandingLoggedIn  ()  {
   const [movieRec, setMovieRec] = useState({})
+  const [tempMovie, setTempMovie] = useState({
+    genreId: '', genreName: ''
+  })
   const [genre, setGenre] = useState('')
-  console.log(movieRec)
+  
+  function handleChange(e){
+    e.preventDefault()
+    const genreValues = e.target.value.split(' ')
+    setTempMovie({genreId: genreValues[0], genreName: genreValues[1]})
+  }
+
   async function handleMovieRequest(e){
     e.preventDefault()
-    console.log(e)
-    // const genre = e.target.value
-    // try {
-    //   let movie = {
-    //     title : '',
-    //     poster_path: '',
-    //   }
-    //   while(!movie.poster_path){
-    //     movie = await movieService.getMovieRec(genre)
-    //   }
-    //   setGenre(e.target.textContent)
-    //   setMovieRec(movie)
-    // } catch (error) {
-    //   console.log(error)
-    // }
+    try {
+      let movie = {
+        title : '',
+        poster_path: '',
+      }
+      while(!movie.poster_path){
+        movie = await movieService.getMovieRec(tempMovie.genreId)
+      }
+      setMovieRec(movie)
+    } catch (error) {
+      console.log(error)
+    }
   }
   return ( 
     <>
@@ -30,26 +59,27 @@ function LandingLoggedIn  ()  {
         <h5>Pick a genre</h5>
 
         <form id='movieId' onSubmit={handleMovieRequest}>
-          <select name='movieId'>
-            <option value='27'>Horror</option>
-            <option value='28'>Action</option>
-            <option value='12'>Adventure</option>
-            <option value='16'>Annimation</option>
-            <option value='35'>Comedy</option>
-            <option value='80'>Crime</option>
-            <option value='99'>Documentary</option>
-            <option value='18'>Drama</option>
-            <option value='10751'>Family</option>
-            <option value='14'>Fantasy</option>
-            <option value='36'>History</option>
-            <option value='10402'>Music</option>
-            <option value='9648'>Mystery</option>
-            <option value='10749'>Romance</option>
-            <option value='878'>Sci Fi</option>
-            <option value='10770'>TV Movie</option>
-            <option value='53'>Thriller</option>
-            <option value='10752'>War</option>
-            <option value='37'>Western</option>
+          <select onChange={handleChange}name='movieId'>
+            <option value={null}>--Select a Genre--</option>
+            <option value='27 Horror'>Horror</option>
+            <option value='28 Action'>Action</option>
+            <option value='12 Adventure'>Adventure</option>
+            <option value='16 Annimation'>Annimation</option>
+            <option value='35 Comedy'>Comedy</option>
+            <option value='80 Crime'>Crime</option>
+            <option value='99 Documentary'>Documentary</option>
+            <option value='18 Drama'>Drama</option>
+            <option value='10751 Family'>Family</option>
+            <option value='14 Fantasy'>Fantasy</option>
+            <option value='36 History'>History</option>
+            <option value='10402 Music'>Music</option>
+            <option value='9648 Mystery'>Mystery</option>
+            <option value='10749 Romance'>Romance</option>
+            <option value='878 SciFi'>Sci Fi</option>
+            <option value='10770 TV-Movie'>TV Movie</option>
+            <option value='53 Thriller'>Thriller</option>
+            <option value='10752 War'>War</option>
+            <option value='37 Western'>Western</option>
           </select>
           <input type='submit' value='search'/>
         </form>
